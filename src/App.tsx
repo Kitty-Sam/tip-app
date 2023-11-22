61195598b6e3472b8915cd2a58d83f1493bdcb89
kittypitty <samutic40@gmail.com>
2023-11-22 10:01:57 +0100

    init: start project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import { MainScreen } from "@components/MainScreen";
+
+export const App = () => {
+  return (
+    <div>
+      <MainScreen />
+    </div>
+  );
+};
---
+++ src/components/MainScreen/index.tsx
@@ -0,0 +1,3 @@
+export const MainScreen = () => {
+  return <div>MainScreen</div>;
+};
